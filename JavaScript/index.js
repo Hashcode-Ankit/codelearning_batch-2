4cfc5175d3f9af7bb610c21cd24cdf676d56121f
Learning Code <Learn@APPLEs-MacBook-Pro.local>
2023-03-10 23:11:56 +0530

    js -1

---
+++ JavaScript/index.js
@@ -0,0 +1,33 @@
+
+
+
+let a = 10
+let b = 2
+
+// console.log(a^b)
+// + operator
+
+// - operator
+
+// / operator
+
+// * operator 
+
+// ** (power operator)
+
+// & operator
+
+
+// | (or) operator
+
+// ^ (xor) operator
+
+a = [10,20,30,"ankit","course"]
+
+
+obj = {
+    "ankit" : 4,
+    "kaushik": 10,
+}
+
+console.log(obj.kaushik)
