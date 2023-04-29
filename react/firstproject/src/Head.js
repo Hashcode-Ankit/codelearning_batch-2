dfd3ce03cccbd51d6b0eeedc4b119ea4258657fc
Learning Code <Learn@APPLEs-MacBook-Pro.local>
2023-04-29 20:58:04 +0530

    react code

--- react/firstproject/src/Head.js
+++ react/firstproject/src/Head.js
@@ -2,25 +2,21 @@ import { useState, useEffect,useContext } from "react";
 import MyContext  from './index'
 
 function Head() {
-    const context = useContext(MyContext)
-    console.log(context)
-    const [count, setCount] = useState(0)
-    
-    function updateCount(){
-        setCount(count+1)
-        console.log("set count called")
-    }
+    const [name,SetName] = useState(["ankit",2])
+    const [data,SetData] = useState("")
+
+    // const currentContext = useContext(MyContext)
     useEffect(() => {
-         console.log("hi")
-    },[]); // use effect same as component did mount // special purpose of this hook is to call api's
-    
+      const headers = { 'Content-Type': 'application/json' }
+      fetch('https://api.npms.io/v2/search?q=react', { headers })
+          .then(response => response.json())
+          .then(data => SetData( JSON.stringify(data.results) )); 
+    },[]);
   return (
     <>
-    <h1>Here is the context value {context[1]}</h1>
-    <h2> Here will be our data {count}</h2>
-    <button onClick={updateCount}>
-        Click me
-      </button>
+    <h1>Here is value {name[0]} {name[1]}</h1>
+    <h2>{data}</h2>
+    <button onClick={()=>SetName(["saurav",2])}>Click Me</button>
     </>
   );
 }
@@ -29,3 +25,39 @@ export default Head;
 
 // call the api get the data 
 // update other component by passing the data using context
+
+
+
+// code for ease 
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="blogs" element={<Blogs />} />
+//           <Route path="contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+
+
+// const headers = { 'Content-Type': 'application/json' }
+//     fetch('https://api.npms.io/v2/search?q=react', { headers })
+//         .then(response => response.json())
+//         .then(data => this.setState({ totalReactPackages: data.total }));
--- react/firstproject/src/index.js
+++ react/firstproject/src/index.js
@@ -1,5 +1,6 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import Head from './Head'
@@ -8,13 +9,28 @@ let MyContext = createContext()
 
 let user  = ["username","password"]
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( // render method only retuns one tag and that is why for one tag purpose we use fragments
+  <>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/head" element={<Head />} />
+        <Route path="/index" element={<App />} />
+      </Routes>
+     </BrowserRouter> 
+      <li><a href ="/head">Head</a></li>
+      <li><a href ="/index">App</a></li> 
+  </>
   // <App color = "red"/>
-  <MyContext.Provider value = {user} >
-     <Head/>
-  </MyContext.Provider>
+  // <MyContext.Provider value = {user} >
+  //    <Head/>
+  // </MyContext.Provider>
  
 );
 
 
 export default MyContext;
+
+
+// In the index.js call the api using useEffect hook 
+// Pass this data using myContext to the child compone userInfo
+// Show this on card
